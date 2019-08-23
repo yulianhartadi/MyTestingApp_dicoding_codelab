897b05523108410e379e81aac5c38833e7915938
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 16:40:01 +0700

    simulate error application like :  Adding big image to simulate error of "Out Of Memory Exception" and "Application is not responding"

--- app/src/main/java/com/example/rdstudio/mytestingapp/MainActivity.java
+++ app/src/main/java/com/example/rdstudio/mytestingapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.rdstudio.mytestingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -14,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnSetValue;
     private TextView tvText;
     private ArrayList<String> names;
+
+    ImageView imgPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         names.add("Narenda Wicaksono");
         names.add("Kevin");
         names.add("Yoza");
+
+        // Adding big image to simulate error of "Out Of Memory Exception
+        //imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.Fronalpstock_big));
+
+        // Solution to error above is use 3rd party library like Glide library
+        // https://github.com/bumptech/glide
+        Glide.with(this).load(R.drawable.Fronalpstock_big).into(imgPreview);
     }
 
     @Override
@@ -40,10 +55,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tvText.setText(name.toString());
 
             // try to add thread sleep to simulate error Application Not Responding
+            /*try {
+                Thread.sleep(3000000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }*/
+
+
+        }
+    }
+
+    private static class delayAsync extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
             try {
                 Thread.sleep(3000000);
             } catch (InterruptedException e){
                 e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d("Delay Async", "Done");
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Log.d("Delay Async", "Cancelled");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (delayAsync != null){
+            if (delayAsync.getStatus().equals(AsyncTask.Status.RUNNING)){
+                delayAsync.cancel(true);
             }
         }
     }
