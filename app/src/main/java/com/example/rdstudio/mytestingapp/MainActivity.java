cca85ea35c5457c4b72d884ab37b05bf5820e0a0
RawzaDigital <rawzadigital@gmail.com>
2019-08-23 13:38:17 +0700

    harusnya error null pointer exception tapi enggak

--- app/src/main/java/com/example/rdstudio/mytestingapp/MainActivity.java
+++ app/src/main/java/com/example/rdstudio/mytestingapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.rdstudio.mytestingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSetValue;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSetValue = findViewById(R.id.btn_set_value);
+
+        tvText = findViewById(R.id.tv_text);
+        btnSetValue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.btn_set_value){
+            tvText.setText("19");
+        }
     }
 }
